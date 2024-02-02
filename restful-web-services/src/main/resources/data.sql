c53d70df68f3efd574f7c048f1f0adc52aef2d2a
Rushikesh Khurpe <lit2020049@iiitl.ac.in>
2024-02-02 16:41:05 +0530

    first commit

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, task, target_date, done)
+values(10001, 'Rushikesh', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username, task, target_date, done)
+values(10002, 'Rushikesh', 'Integrate JWT', sysdate(), false);
+
+insert into todo(id, username, task, target_date, done)
+values(10003, 'Rushikesh', 'Fix UI', sysdate(), false);
